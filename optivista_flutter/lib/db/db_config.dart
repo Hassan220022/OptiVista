73d35f0a6d4ba3a100b4936e7ebda6de16332e3f
Hassan220022 <hassansonson2002@gmail.com>
2024-12-27 03:51:21 +0200

    some changes

--- optivista_flutter/lib/db/db_config.dart
+++
@@ -1,7 +0,0 @@
-class DbConfig {
-  static const String host = '196.221.151.195';
-  static const int port = 3306;
-  static const String user = 'dev';
-  static const String password = 'DevBor3i238\$\$';
-  static const String database = 'ecom_db';
-}
