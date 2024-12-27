4907153cf4d12ec2d87689d1fe3cd93b1ddd2ada
Hassan220022 <hassansonson2002@gmail.com>
2024-12-27 03:53:30 +0200

    Remove unused imports from cart_screen_widget and flutter_flow_util

--- optivista_flutter/lib/cart_screen/cart_screen_widget.dart
+++ optivista_flutter/lib/cart_screen/cart_screen_widget.dart
@@ -3,7 +3,6 @@ import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'cart_screen_model.dart';
 export 'cart_screen_model.dart';
 
