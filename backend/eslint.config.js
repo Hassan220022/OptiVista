5a1260b684b31d974abcf7824f97b0cea650a053
Hassan220022 <hassansonson2002@gmail.com>
2024-12-27 06:52:18 +0200

    Update ESLint configuration to support React and add module type in package.json

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,27 +1,28 @@
-export default [
-    {
-      // Specify the environments your script is designed to run in
-      languageOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true, // If you're using JSX
-        },
+// eslint.config.js
+module.exports = [
+  {
+    // Specify the environments your script is designed to run in
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true, // If you're using JSX
       },
-      // Extend recommended ESLint rules
-      extends: ['eslint:recommended'],
-      // Define plugins if needed
-      plugins: {
-        // Example: 'react': require('eslint-plugin-react'),
-      },
-      // Specify custom rules
-      rules: {
-        semi: ['error', 'always'],
-        quotes: ['error', 'single'],
-        'no-unused-vars': ['warn'],
-        // Add more custom rules as needed
-      },
-      // Ignore specific files or directories
-      ignores: ['node_modules/', 'dist/', 'src/.DS_Store', '.DS_Store'],
     },
-  ];
+    // Extend recommended ESLint rules
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    // Define plugins if needed
+    plugins: {
+      react: require('eslint-plugin-react'),
+    },
+    // Specify custom rules
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-unused-vars': ['warn'],
+      // Add more custom rules as needed
+    },
+    // Ignore specific files or directories
+    ignores: ['node_modules/', 'dist/', 'src/.DS_Store', '.DS_Store'],
+  },
+];
